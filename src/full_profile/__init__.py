b20fbb87924c1d93680e2d24c13a4f471421c52b
Roger <rogermoens@delfius.nl>
2024-09-30 11:38:34 +0200

    Initialization

---
+++ src/full_profile/__init__.py
@@ -0,0 +1,10 @@
+"""Full-Profile Analysis"""
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("full_profile")
+except PackageNotFoundError:
+    __version__ = "uninstalled"
+
+__author__ = "Roger A.R. Moens"
+__email__ = "r.a.r.moens@tudelft.nl"
---
+++ src/full_profile/fpc.py
@@ -0,0 +1,316 @@
+import builtins
+import time
+import numpy as np
+import scipy.sparse as ss
+import cupyx.scipy.sparse as cpss
+from unipy import *
+from typing import List, NoReturn, Union
+from joblib import Parallel, delayed
+
+from unipy import types as ty
+
+
+class fpc:
+    def __init__(
+        self, 
+        maxiter: int = 10, 
+        tau_factor: float = 0.05, 
+        delta: float = 1,
+        sv_factor: float = 0.05,
+        dense : bool = True,
+        stop_criteria : float = 0.05,
+        verbose: bool = False,
+        **kwargs,
+    ) -> NoReturn:
+        """
+        Initialize the SVT object.
+
+        Parameters:
+        - maxiter: int
+            Maximum number of iterations.
+        - tau_factor: float
+            Multiplicative factor for the threshold parameter tau.
+        - delta: float
+            Step size for the update of the augmented Lagrangian variable Y.
+
+        Returns:
+            None
+        """
+        self.maxiter = maxiter
+        self.tau_factor = tau_factor
+        self.delta = delta
+        self.sv_factor = sv_factor
+        self.dense = dense
+        self.stop_criteria = stop_criteria
+        self.verbose = verbose
+        self.kwargs = kwargs
+        self.tic = 0
+        
+        pass
+    
+    @property
+    def b(self) -> ty.Array:
+        """Reconstruct b (low-rank component) from svd components
+
+        Returns
+        -------
+            array : reconstructed low-rank component
+
+        """
+        return matmul(multiply(np.array(self._b[0]), np.array(self._b[1])), np.array(self._b[2]))
+    
+    @b.setter
+    def b(self, a: tuple[ty.Array]) -> None:
+        """Sets b from tuple
+
+        Parameters
+        ----------
+        a : tuple[array]
+            Tuple containing arrays of svd to set b
+
+        """
+        # print(type(self._b[1]))
+        self._b[0] = a[0]
+        self._b[1] = a[1]
+        self._b[2] = a[2]
+        return None
+    
+    def _initialize_b(self) -> None:
+        """Initialize b"""
+        self._b = [0, 0, 0]
+        self._b[0] = zeros(
+            (self._m, 1), atype=self.btype, dtype=self.datatype
+        )
+        self._b[1] = zeros((1, 1), atype=self.btype, dtype=self.datatype)
+        self._b[2] = zeros(
+            (1, self._n), atype=self.btype, dtype=self.datatype
+        )
+        return None
+    
+    def _datatype(self, dtype: str) -> str:
+        """Returns datatype and promotes if required"""
+        dtype = dtype if dtype not in ['int8', 'int16', 'int32', 'int64'] else 'float32'
+        
+        return dtype
+
+
+    def initialize(self, a: ty.Array) -> None:
+        """
+        Initialize the SVT algorithm.
+
+        Parameters:
+        None
+
+        Returns:
+        None
+        """
+        tic = time.time()
+        self.a = a
+        self.datatype = self._datatype(str(a.dtype))
+        self._m, self._n = self.a.shape
+        # Initialize variables
+        self.atype = (
+            "scipy.sparse"
+            if find_package(self.a)[1] == "numpy" or find_package(self.a)[1] == "scipy.sparse"
+            else "cupyx.scipy.sparse"
+        )
+        
+        self.btype = (
+            "numpy"
+            if find_package(self.a)[1] == "numpy" or find_package(self.a)[1] == "scipy.sparse"
+            else "cupy"
+        )
+
+        self._ss = (
+            ss
+            if find_package(self.a)[1] == "numpy" or find_package(self.a)[1] == "scipy.sparse"
+            else cpss
+        )
+
+        self._initialize_b()
+
+        #self.y = self._ss.csc_matrix((np.zeros_like(self.a.data), self.a.indices, self.a.indptr))
+        # make Y a dense matrix
+        self.y = zeros(
+            (self._m, self._n), atype=self.btype, dtype=self.datatype
+        )
+        
+        #zeros((self._m, self._n), atype=self.atype, dtype=self.datatype)
+        self.x_sparse = self._ss.csc_matrix((np.zeros_like(self.a.data), self.a.indices, self.a.indptr), shape=(self._m, self._n))
+        #zeros((self._m, self._n), atype=self.atype, dtype=self.datatype)
+        self.tau = self.tau_factor * builtins.max(self.a.shape)
+        self.a_fro = linalg.norm(self.a.data, 2)
+        self.k0 = 1
+        self.k = 0
+        self._sv = 10  # set sv
+        #print('Initialization', time.time() - tic)
+        return None
+
+
+    def stopping_criterion(self) -> bool:
+        """
+        Check the stopping criterion.
+
+        Parameters:
+        None
+
+        Returns:
+        - bool
+            True if the stopping criterion is met, False otherwise.
+        """
+        crit = linalg.norm((self.a.data - self.x_sparse.data), 2) / self.a_fro
+        if crit < self.stop_criteria:
+            if self.verbose:
+                print('Iteration ' + str(self.k) + ' - Crit = ' + str(crit) + ' - Rank = ' + str(self._svp) + ' - Time = '+ str(time.time()-self.tic))
+            return True
+        else:
+            if self.verbose:
+                print('Iteration ' + str(self.k) + ' - Crit = ' + str(crit) + ' - Rank = ' + str(self._svp) + ' - Time = '+ str(time.time()-self.tic))
+            return False
+
+        
+    def soft_thresholding(self) -> None:
+        """Update b by performing truncated svd"""
+        # print('\n Soft Threshold\n')
+        if "method" in self.kwargs:
+            sv = (
+                self._sv + 10 if linalg.requires_sv(self.kwargs["method"]) is True else 0
+            )
+            if self.k > 0:
+                if self.kwargs["method"] == 'sparse_propack':
+                    self.kwargs.update(
+                        {"v0": self._b[2][0,:] if sum(self._b[1]) > 1e-2 else None}
+                    )
+                else:
+                    if self._b[0].shape[0] < self._b[2].shape[1]:
+                        self.kwargs.update(
+                            {"v0": self._b[0][:,0] if sum(self._b[1]) > 1e-2 else None}
+                        )
+                    else:
+                        self.kwargs.update(
+                            {"v0": self._b[2][0,:] if sum(self._b[1]) > 1e-2 else None}
+                        )               
+
+        else:
+            sv = 0
+
+        self.kwargs.update({"sv": sv})
+        
+        self.b = linalg.svd(self.y, self.kwargs)
+        #print(type(self._b[0]), self._b[1])#, self._b[1])
+        self._svp = int(sum(self._b[1] > self.tau))
+        if self._svp != 0:
+            self._threshold(self._svp, self.tau)
+        else:  # if svp is 0, just reset the svd result
+            self._initialize_b()
+        return None  
+    
+    def _threshold(self, num: int = 1, value: float = 0.0) -> None:
+        """Threshold b to certain value
+
+        Parameters
+        ----------
+        num: int
+            Truncate to this number of values
+
+        value : float
+            Threshold to this value
+
+        """
+
+        self._b[0] = self._b[0][:, :num]
+        self._b[1] = self._b[1][:num]
+        self._b[1] -= value
+        self._b[2] = self._b[2][:num, :]
+
+
+        return None
+
+    def _update_sv(self) -> None:
+        """Update (predict) the number of singular values to be calculated by svd"""
+        n = min(self._m, self._n)
+        if self._svp < self._sv:
+            self._sv = int(min(self._svp + 1, n))
+        else:
+            self._sv = int(min(self._svp + 10, n))
+    
+    def update_x_sparse(self) -> None:
+        # self.x_sparse.data = np.einsum(
+        #     'ij,jj,ji->i', 
+        #     self._b[0][self.a.row,:], 
+        #     self._b[1], 
+        #     self._b[2][:, self.a.col], 
+        # optimize='greedy')
+
+        #self.x_sparse.data = sum((to_arraytype(self._b[0], self.btype)[self.x_sparse.row,:]*to_arraytype(self._b[1], self.btype))*to_arraytype((self._b[2]).T, self.btype)[self.x_sparse.col,:], axis=1)
+        u = to_arraytype(self._b[0], self.btype)*to_arraytype(self._b[1], self.btype)
+        v = to_arraytype(self._b[2], self.btype)
+
+        if self.dense:
+            uv = u@v
+        
+        for i in range(self.a.shape[1]):
+            col = i
+            row = self.x_sparse.indices[self.x_sparse.indptr[i]:self.x_sparse.indptr[i+1]] 
+            self.x_sparse.data[self.x_sparse.indptr[i]:self.x_sparse.indptr[i+1]] = uv[row,col] if self.dense else u[row,:]@v[:, col]
+    
+        #self.x_sparse.data = sum((to_arraytype(self._b[0], self.btype)[self.x_sparse.row,:]*to_arraytype(self._b[1], self.btype))*to_arraytype((self._b[2]).T, self.btype)[self.x_sparse.col,:], axis=1)
+        
+        return None
+        
+    def update_y(self):
+        """
+        Update the augmented Lagrangian variable Y.
+
+        Parameters:
+        None
+
+        Returns:
+        None
+        """
+        #print(self.y.shape, self.b.shape, self.a.shape, self.x_sparse.shape)
+        self.y = self.b + self.delta * (self.a - self.x_sparse)
+        
+
+    def update_delta(self):
+        """
+        Update the step size delta if needed.
+
+        Parameters:
+        None
+
+        Returns:
+        None
+        """
+        self.delta *= 1
+        return self.delta
+
+    
+    def run(self, a: ty.Array) -> None:
+        """
+        Execute the SVT algorithm.
+
+        Parameters:
+        None
+
+        Returns:
+        None
+        """
+        self.initialize(a)
+        self.tic = time.time()
+        
+        while self.k < self.maxiter:
+            self.soft_thresholding()
+            self._update_sv()
+            self.update_x_sparse()
+
+            if self.stopping_criterion():
+                break
+
+            self.tic = time.time()
+            self.update_y()
+            self.update_delta()
+            
+            self.k += 1
+
+        return None
