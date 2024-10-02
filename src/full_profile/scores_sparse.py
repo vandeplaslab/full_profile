f465641647deffdfb5fc9ca8669527b11189f513
lukasz-migas <lukas.migas@yahoo.com>
2024-10-02 16:42:38 +0100

    Cleanup

--- src/full_profile/scores_sparse.py
+++ src/full_profile/scores_sparse.py
@@ -1,142 +1,149 @@
-from typing import Any
-import numpy as np
-from unipy import *
-import scipy.sparse as ss
-from joblib import Parallel, delayed
+"""Scores for sparse matrices."""
 
-class scores_sparse:
+import typing as ty
+
+import numpy as np
+from joblib import Parallel, delayed
+from unipy import linalg
+
+
+class SparseScores:
+    """Sparse scores."""
+
     def __init__(
-        self, 
-        reference: Any,
-        factored: Any,
-        ):
+        self,
+        reference: ty.Any,
+        factored: ty.Any,
+    ):
         self.reference_data = reference[0]
         self.reference_indices = reference[1]
         self.reference_indptr = reference[2]
         self.reference_shape = reference[3]
         self.factored = factored
-        pass
 
-#     def densify(self, s):
-#         return np.array(s.todense())
-    
-#     def score_peak_selection(
-#         self, 
-#         nr_peaks: int = 100
-#     ) -> Any:
-#         """
-#         Calculate score for peak selection.
-#         """
-#         total_ion_sum = np.array(self.reference.sum(axis=1))
-#         sort = np.argsort(total_ion_sum.T)
-#         peak_selection = (sort[0])[-nr_peaks:]
+    #     def densify(self, s):
+    #         return np.array(s.todense())
 
-#         selection1 = self.densify(self.reference[peak_selection,:])
-#         selection2 = self.factored[0][peak_selection,:]@self.factored[1]
-#         selection1[selection1<0.1] = np.nan
+    #     def score_peak_selection(
+    #         self,
+    #         nr_peaks: int = 100
+    #     ) -> Any:
+    #         """
+    #         Calculate score for peak selection.
+    #         """
+    #         total_ion_sum = np.array(self.reference.sum(axis=1))
+    #         sort = np.argsort(total_ion_sum.T)
+    #         peak_selection = (sort[0])[-nr_peaks:]
 
-#         score = np.linalg.norm(selection1-selection2, 2, axis=0)/np.linalg.norm(selection1, 2, axis=0)*100
-#         return score
-    
-    
-#     def _peak_match(self, i, nr_peaks):
-#         spectrum = self.densify(self.reference[:, [i]])[:, 0]
-#         compare = to_arraytype(self.factored[0]@self.factored[1][:,[i]], 'numpy')[:, 0]
-#         idx = np.argpartition(spectrum, -nr_peaks)[-nr_peaks:]
-#         score = np.linalg.norm(compare[idx] - spectrum[idx], 2) / np.linalg.norm(spectrum[idx], 2) * 100
-        
-#         return (i, score)
-        
-        
-#     def peak_match_per_spectrum(        
-#         self, 
-#         nr_peaks: int = 100,
-#         n_jobs: int = 2,
-#     ) -> Any:
-#         """
-#         Calculate peak match per spectrum score.
-#         """
-#         score = np.zeros(self.reference.shape[1])
-        
-#         A = Parallel(n_jobs=n_jobs, backend='threading')( # max 51/52
-#             delayed(self._peak_match)(i, nr_peaks) for i in range(self.reference.shape[1])
-#         )
-        
-#         for a in A:
-#             score[a[0]] = a[1]
-        
-        
-#         return np.array(score)
-    
-    
-#     def compression_scores(self, verbose : bool = False) -> tuple:
-#         """
-#         Calculate compression scores.
-#         """
-#         raw_dense_size = np.prod(self.reference.shape) * 4 / (10**9)
-#         raw_sparse_size = (self.reference.data.nbytes + self.reference.indices.nbytes + self.reference.indptr.nbytes) / (10**9)
-#         compressed_size = (np.prod(self.factored[0].shape) + np.prod(self.factored[1].shape)) * 4 / (10**9)
-#         comp1 = raw_dense_size / compressed_size
-#         comp2 = raw_sparse_size / compressed_size
-#         if verbose:
-#             print('Dense vs. Compressed:', comp1, '\nSparse vs. Compressed:', comp2)
-#         return (comp1, comp2)
+    #         selection1 = self.densify(self.reference[peak_selection,:])
+    #         selection2 = self.factored[0][peak_selection,:]@self.factored[1]
+    #         selection1[selection1<0.1] = np.nan
 
-    
-#     def reconstruction_score_old(self, dense : bool = False) -> float: # Something wrong in here
-#         a_fro = linalg.norm(self.reference.data, 2)
-#         x_sparse = ss.csc_matrix((np.zeros_like(self.reference.data), self.reference.indices, self.reference.indptr))
-#         u = self.factored[0]
-#         v = self.factored[1]
+    #         score = np.linalg.norm(selection1-selection2, 2, axis=0)/np.linalg.norm(selection1, 2, axis=0)*100
+    #         return score
 
-#         if dense:
-#             uv = u@v
-        
-#         for i in range(self.reference.shape[1]):
-#             col = i
-#             row = x_sparse.indices[x_sparse.indptr[i]:x_sparse.indptr[i+1]] 
-#             x_sparse.data[x_sparse.indptr[i]:x_sparse.indptr[i+1]] = uv[row,col] if dense else u[row,:]@v[:, col]
-    
-        
-#         return linalg.norm((self.reference.data - x_sparse.data), 2) / a_fro
+    #     def _peak_match(self, i, nr_peaks):
+    #         spectrum = self.densify(self.reference[:, [i]])[:, 0]
+    #         compare = to_arraytype(self.factored[0]@self.factored[1][:,[i]], 'numpy')[:, 0]
+    #         idx = np.argpartition(spectrum, -nr_peaks)[-nr_peaks:]
+    #         score = np.linalg.norm(compare[idx] - spectrum[idx], 2) / np.linalg.norm(spectrum[idx], 2) * 100
 
-#     def _reconstruction(self, i): 
-#         col = i
-#         row = self.reference.indices[self.reference.indptr[i]:self.reference.indptr[i+1]]
-#         #self.x_sparse.data[self.x_sparse.indptr[col]:self.x_sparse.indptr[col+1]] = self.factored[0][row, :]@self.factored[1][:, col]
-#         #data = self.factored[0][row, :]@self.factored[1][:, col]        
-        
-#         return self.factored[0][row, :]@self.factored[1][:, col]
-    
-#     def reconstruction_score(self, n_jobs : int = 2) -> float:
-#         a_fro = linalg.norm(self.reference_data, 2)
+    #         return (i, score)
 
-#         A = Parallel(n_jobs=n_jobs)( # max 51/52 
-#             delayed(self._reconstruction)(i) for i in range(self.reference.shape[1])
-#         )
-        
-#         x_sparse_data = np.concatenate(A, dtype='float32')
-#         # for a in A:
-#         #     col = a[0]
-#         #     row = self.x_sparse.indices[self.x_sparse.indptr[col]:self.x_sparse.indptr[col+1]] 
-#         #     self.x_sparse.data[self.x_sparse.indptr[col]:self.x_sparse.indptr[col+1]] = a[1]
-            
-#         return linalg.norm((self.reference.data - x_sparse_data), 2) / a_fro
-    
-    
+    #     def peak_match_per_spectrum(
+    #         self,
+    #         nr_peaks: int = 100,
+    #         n_jobs: int = 2,
+    #     ) -> Any:
+    #         """
+    #         Calculate peak match per spectrum score.
+    #         """
+    #         score = np.zeros(self.reference.shape[1])
+
+    #         A = Parallel(n_jobs=n_jobs, backend='threading')( # max 51/52
+    #             delayed(self._peak_match)(i, nr_peaks) for i in range(self.reference.shape[1])
+    #         )
+
+    #         for a in A:
+    #             score[a[0]] = a[1]
+
+    #         return np.array(score)
+
+    #     def compression_scores(self, verbose : bool = False) -> tuple:
+    #         """
+    #         Calculate compression scores.
+    #         """
+    #         raw_dense_size = np.prod(self.reference.shape) * 4 / (10**9)
+    #         raw_sparse_size = (self.reference.data.nbytes + self.reference.indices.nbytes + self.reference.indptr.nbytes) / (10**9)
+    #         compressed_size = (np.prod(self.factored[0].shape) + np.prod(self.factored[1].shape)) * 4 / (10**9)
+    #         comp1 = raw_dense_size / compressed_size
+    #         comp2 = raw_sparse_size / compressed_size
+    #         if verbose:
+    #             print('Dense vs. Compressed:', comp1, '\nSparse vs. Compressed:', comp2)
+    #         return (comp1, comp2)
+
+    #     def reconstruction_score_old(self, dense : bool = False) -> float: # Something wrong in here
+    #         a_fro = linalg.norm(self.reference.data, 2)
+    #         x_sparse = ss.csc_matrix((np.zeros_like(self.reference.data), self.reference.indices, self.reference.indptr))
+    #         u = self.factored[0]
+    #         v = self.factored[1]
+
+    #         if dense:
+    #             uv = u@v
+
+    #         for i in range(self.reference.shape[1]):
+    #             col = i
+    #             row = x_sparse.indices[x_sparse.indptr[i]:x_sparse.indptr[i+1]]
+    #             x_sparse.data[x_sparse.indptr[i]:x_sparse.indptr[i+1]] = uv[row,col] if dense else u[row,:]@v[:, col]
+
+    #         return linalg.norm((self.reference.data - x_sparse.data), 2) / a_fro
+
+    #     def _reconstruction(self, i):
+    #         col = i
+    #         row = self.reference.indices[self.reference.indptr[i]:self.reference.indptr[i+1]]
+    #         #self.x_sparse.data[self.x_sparse.indptr[col]:self.x_sparse.indptr[col+1]] = self.factored[0][row, :]@self.factored[1][:, col]
+    #         #data = self.factored[0][row, :]@self.factored[1][:, col]
+
+    #         return self.factored[0][row, :]@self.factored[1][:, col]
+
+    #     def reconstruction_score(self, n_jobs : int = 2) -> float:
+    #         a_fro = linalg.norm(self.reference_data, 2)
+
+    #         A = Parallel(n_jobs=n_jobs)( # max 51/52
+    #             delayed(self._reconstruction)(i) for i in range(self.reference.shape[1])
+    #         )
+
+    #         x_sparse_data = np.concatenate(A, dtype='float32')
+    #         # for a in A:
+    #         #     col = a[0]
+    #         #     row = self.x_sparse.indices[self.x_sparse.indptr[col]:self.x_sparse.indptr[col+1]]
+    #         #     self.x_sparse.data[self.x_sparse.indptr[col]:self.x_sparse.indptr[col+1]] = a[1]
+
+    #         return linalg.norm((self.reference.data - x_sparse_data), 2) / a_fro
+
     def _reconstruction(self, i):
         col = i
-        row = self.reference_indices[self.reference_indptr[i]:self.reference_indptr[i+1]]     
-        return np.linalg.norm(self.factored[0][row, :]@self.factored[1][:, col]-self.reference_data[self.reference_indptr[i]:self.reference_indptr[i+1]], 2)**2
+        row = self.reference_indices[self.reference_indptr[i] : self.reference_indptr[i + 1]]
+        return (
+            np.linalg.norm(
+                self.factored[0][row, :] @ self.factored[1][:, col]
+                - self.reference_data[self.reference_indptr[i] : self.reference_indptr[i + 1]],
+                2,
+            )
+            ** 2
+        )
 
-    
-    def reconstruction_score(self, n_jobs : int = 2) -> float:
+    def reconstruction_score(self, n_jobs: int = 2) -> float:
         a_fro = linalg.norm(self.reference_data, 2)
 
-        A = Parallel(n_jobs=n_jobs)( # max 51/52 
+        A = Parallel(n_jobs=n_jobs)(  # max 51/52
             delayed(self._reconstruction)(i) for i in range(self.reference_shape[1])
         )
-        
-        #A = np.sum(np.array(A))
 
-        return np.array(A), a_fro#np.sqrt(A) / a_fro
+        # A = np.sum(np.array(A))
+
+        return np.array(A), a_fro  # np.sqrt(A) / a_fro
+
+
+# for back compatibility with previous versions
+scores_sparse = SparseScores
--- src/full_profile/svt.py
+++ src/full_profile/svt.py
@@ -1,29 +1,31 @@
-import builtins
+"""SVT."""
+
 import time
+import typing as ty
+
+import cupyx.scipy.sparse as cpss
 import numpy as np
 import scipy.sparse as ss
-import cupyx.scipy.sparse as cpss
-from unipy import *
-from typing import List, NoReturn, Union
-from joblib import Parallel, delayed
+import unipy.types as uty
+from unipy import find_package, linalg, matmul, multiply, to_arraytype, zeros
 
-from unipy import types as ty
 
-class svt:
+class SVT:
     def __init__(
-        self, 
-        maxiter: int = 10, 
-        tau_factor: float = 0.05, 
+        self,
+        maxiter: int = 10,
+        tau_factor: float = 0.05,
         delta: float = 1,
         sv_factor: float = 0.05,
-        dense : bool = True,
+        dense: bool = True,
         verbose: bool = False,
         **kwargs,
-    ) -> NoReturn:
+    ) -> ty.NoReturn:
         """
         Initialize the SVT object.
 
-        Parameters:
+        Parameters
+        ----------
         - maxiter: int
             Maximum number of iterations.
         - tau_factor: float
@@ -31,7 +33,8 @@ class svt:
         - delta: float
             Step size for the update of the augmented Lagrangian variable Y.
 
-        Returns:
+        Returns
+        -------
             None
         """
         self.maxiter = maxiter
@@ -42,12 +45,12 @@ class svt:
         self.verbose = verbose
         self.kwargs = kwargs
         self.tic = 0
-        
+
         pass
-    
+
     @property
-    def b(self) -> ty.Array:
-        """Reconstruct b (low-rank component) from svd components
+    def b(self) -> uty.Array:
+        """Reconstruct b (low-rank component) from svd components.
 
         Returns
         -------
@@ -55,10 +58,10 @@ class svt:
 
         """
         return matmul(multiply(self._b[0].todense(), self._b[1].todense()), self._b[2].todense())
-    
+
     @b.setter
-    def b(self, a: tuple[ty.Array]) -> None:
-        """Sets b from tuple
+    def b(self, a: tuple[uty.Array]) -> None:
+        """Sets b from tuple.
 
         Parameters
         ----------
@@ -71,37 +74,34 @@ class svt:
         self._b[1] = a[1]
         self._b[2] = a[2]
         return None
-    
+
     def _initialize_b(self) -> None:
-        """Initialize b"""
+        """Initialize b."""
         self._b = [0, 0, 0]
-        self._b[0] = zeros(
-            (self._m, 1), atype=self.atype, dtype=self.datatype
-        )
+        self._b[0] = zeros((self._m, 1), atype=self.atype, dtype=self.datatype)
         self._b[1] = zeros((1, 1), atype=self.atype, dtype=self.datatype)
-        self._b[2] = zeros(
-            (1, self._n), atype=self.atype, dtype=self.datatype
-        )
+        self._b[2] = zeros((1, self._n), atype=self.atype, dtype=self.datatype)
         return None
-    
+
     def _datatype(self, dtype: str) -> str:
-        """Returns datatype and promotes if required"""
-        dtype = dtype if dtype not in ['int8', 'int16', 'int32', 'int64'] else 'float32'
-        
+        """Returns datatype and promotes if required."""
+        dtype = dtype if dtype not in ["int8", "int16", "int32", "int64"] else "float32"
+
         return dtype
 
-
-    def initialize(self, a: ty.Array) -> None:
+    def initialize(self, a: uty.Array) -> None:
         """
         Initialize the SVT algorithm.
 
-        Parameters:
+        Parameters
+        ----------
         None
 
-        Returns:
+        Returns
+        -------
         None
         """
-        tic = time.time()
+        time.time()
         self.a = a
         self.datatype = self._datatype(str(a.dtype))
         self._m, self._n = self.a.shape
@@ -111,26 +111,24 @@ class svt:
             if find_package(self.a)[1] == "numpy" or find_package(self.a)[1] == "scipy.sparse"
             else "cupyx.scipy.sparse"
         )
-        
+
         self.btype = (
-            "numpy"
-            if find_package(self.a)[1] == "numpy" or find_package(self.a)[1] == "scipy.sparse"
-            else "cupy"
+            "numpy" if find_package(self.a)[1] == "numpy" or find_package(self.a)[1] == "scipy.sparse" else "cupy"
         )
 
-        self._ss = (
-            ss
-            if find_package(self.a)[1] == "numpy" or find_package(self.a)[1] == "scipy.sparse"
-            else cpss
-        )
+        self._ss = ss if find_package(self.a)[1] == "numpy" or find_package(self.a)[1] == "scipy.sparse" else cpss
 
         self._initialize_b()
 
-        self.y = self._ss.csc_matrix((np.zeros_like(self.a.data), self.a.indices, self.a.indptr), shape=(self._m, self._n))
-        #zeros((self._m, self._n), atype=self.atype, dtype=self.datatype)
-        self.x_sparse = self._ss.csc_matrix((np.zeros_like(self.a.data), self.a.indices, self.a.indptr), shape=(self._m, self._n))
-        #zeros((self._m, self._n), atype=self.atype, dtype=self.datatype)
-        self.tau = self.tau_factor * builtins.max(self.a.shape)
+        self.y = self._ss.csc_matrix(
+            (np.zeros_like(self.a.data), self.a.indices, self.a.indptr), shape=(self._m, self._n)
+        )
+        # zeros((self._m, self._n), atype=self.atype, dtype=self.datatype)
+        self.x_sparse = self._ss.csc_matrix(
+            (np.zeros_like(self.a.data), self.a.indices, self.a.indptr), shape=(self._m, self._n)
+        )
+        # zeros((self._m, self._n), atype=self.atype, dtype=self.datatype)
+        self.tau = self.tau_factor * max(self.a.shape)
         self.a_fro = linalg.norm(self.a.data, 2)
         self.k0 = 1
         self.k = 0
@@ -138,52 +136,68 @@ class svt:
         # print('Initialization', time.time() - tic)
         return None
 
-
     def stopping_criterion(self) -> bool:
         """
         Check the stopping criterion.
 
-        Parameters:
+        Parameters
+        ----------
         None
 
-        Returns:
+        Returns
+        -------
         - bool
             True if the stopping criterion is met, False otherwise.
         """
         crit = linalg.norm((self.a.data - self.x_sparse.data), 2) / self.a_fro
         if crit < 0.05:
             if self.verbose:
-                print('Iteration ' + str(self.k) + ' - Crit = ' + str(crit) + ' - Rank = ' + str(self._svp) + ' - Time = '+ str(time.time()-self.tic))
+                print(
+                    "Iteration "
+                    + str(self.k)
+                    + " - Crit = "
+                    + str(crit)
+                    + " - Rank = "
+                    + str(self._svp)
+                    + " - Time = "
+                    + str(time.time() - self.tic)
+                )
             return True
         else:
             if self.verbose:
-                print('Iteration ' + str(self.k) + ' - Crit = ' + str(crit) + ' - Rank = ' + str(self._svp) + ' - Time = '+ str(time.time()-self.tic))
+                print(
+                    "Iteration "
+                    + str(self.k)
+                    + " - Crit = "
+                    + str(crit)
+                    + " - Rank = "
+                    + str(self._svp)
+                    + " - Time = "
+                    + str(time.time() - self.tic)
+                )
                 self.tic = time.time()
             return False
 
-        
     def soft_thresholding(self) -> None:
-        """Update b by performing truncated svd"""
+        """Update b by performing truncated svd."""
         # print('\n Soft Threshold\n')
-        tic = time.time()
+        time.time()
         if "method" in self.kwargs:
-            sv = (
-                self._sv + 10 if linalg.requires_sv(self.kwargs["method"]) is True else 0
-            )
+            sv = self._sv + 10 if linalg.requires_sv(self.kwargs["method"]) is True else 0
             if self.k > 0:
-                if self.kwargs["method"] == 'sparse_propack':
+                if self.kwargs["method"] == "sparse_propack":
                     self.kwargs.update(
-                        {"v0": (self._b[2][[0],:].todense())[0,:] if sum(self._b[1].data) > 1e-2 else None}
+                        {"v0": (self._b[2][[0], :].todense())[0, :] if sum(self._b[1].data) > 1e-2 else None}
                     )
                 else:
                     if self._b[0].shape[0] < self._b[2].shape[1]:
                         self.kwargs.update(
-                            {"v0": (self._b[0][:,[0]].todense())[:,0] if sum(self._b[1].data) > 1e-2 else None}
+                            {"v0": (self._b[0][:, [0]].todense())[:, 0] if sum(self._b[1].data) > 1e-2 else None}
                         )
                     else:
                         self.kwargs.update(
-                            {"v0": (self._b[2][[0],:].todense())[0,:] if sum(self._b[1].data) > 1e-2 else None}
-                        )               
+                            {"v0": (self._b[2][[0], :].todense())[0, :] if sum(self._b[1].data) > 1e-2 else None}
+                        )
 
         else:
             sv = 0
@@ -191,17 +205,17 @@ class svt:
         self.kwargs.update({"sv": sv})
 
         self.b = linalg.svd(self.y, self.kwargs)
-        #print(self._b[1].data)#, self._b[1])
+        # print(self._b[1].data)#, self._b[1])
         self._svp = int(sum(self._b[1].data > self.tau))
         if self._svp != 0:
             self._threshold(self._svp, self.tau)
         else:  # if svp is 0, just reset the svd result
             self._initialize_b()
         # print('soft_thresholding', time.time() - tic)
-        return None  
-    
+        return None
+
     def _threshold(self, num: int = 1, value: float = 0.0) -> None:
-        """Threshold b to certain value
+        """Threshold b to certain value.
 
         Parameters
         ----------
@@ -212,86 +226,90 @@ class svt:
             Threshold to this value
 
         """
-
         self._b[0] = self._b[0][:, :num]
         self._b[1] = self._b[1][[0], :num]
         self._b[1].data -= value
         self._b[2] = self._b[2][:num, :]
 
-
         return None
 
     def _update_sv(self) -> None:
-        """Update (predict) the number of singular values to be calculated by svd"""
+        """Update (predict) the number of singular values to be calculated by svd."""
         n = min(self._m, self._n)
         if self._svp < self._sv:
             self._sv = int(min(self._svp + 1, n))
         else:
             self._sv = int(min(self._svp + 10, n))
-    
+
     def update_x_sparse(self) -> None:
-        tic = time.time()
+        time.time()
         # self.x_sparse.data = np.einsum(
-        #     'ij,jj,ji->i', 
-        #     self._b[0][self.a.row,:], 
-        #     self._b[1], 
-        #     self._b[2][:, self.a.col], 
+        #     'ij,jj,ji->i',
+        #     self._b[0][self.a.row,:],
+        #     self._b[1],
+        #     self._b[2][:, self.a.col],
         # optimize='greedy')
 
-        #self.x_sparse.data = sum((to_arraytype(self._b[0], self.btype)[self.x_sparse.row,:]*to_arraytype(self._b[1], self.btype))*to_arraytype((self._b[2]).T, self.btype)[self.x_sparse.col,:], axis=1)
-        u = to_arraytype(self._b[0], self.btype)*to_arraytype(self._b[1], self.btype)
+        # self.x_sparse.data = sum((to_arraytype(self._b[0], self.btype)[self.x_sparse.row,:]*to_arraytype(self._b[1], self.btype))*to_arraytype((self._b[2]).T, self.btype)[self.x_sparse.col,:], axis=1)
+        u = to_arraytype(self._b[0], self.btype) * to_arraytype(self._b[1], self.btype)
         v = to_arraytype(self._b[2], self.btype)
 
         if self.dense:
-            uv = u@v
-        
+            uv = u @ v
+
         for i in range(self.a.shape[1]):
             col = i
-            row = self.x_sparse.indices[self.x_sparse.indptr[i]:self.x_sparse.indptr[i+1]] 
-            self.x_sparse.data[self.x_sparse.indptr[i]:self.x_sparse.indptr[i+1]] = uv[row,col] if self.dense else u[row,:]@v[:, col]
-    
-        #self.x_sparse.data = sum((to_arraytype(self._b[0], self.btype)[self.x_sparse.row,:]*to_arraytype(self._b[1], self.btype))*to_arraytype((self._b[2]).T, self.btype)[self.x_sparse.col,:], axis=1)
-        
+            row = self.x_sparse.indices[self.x_sparse.indptr[i] : self.x_sparse.indptr[i + 1]]
+            self.x_sparse.data[self.x_sparse.indptr[i] : self.x_sparse.indptr[i + 1]] = (
+                uv[row, col] if self.dense else u[row, :] @ v[:, col]
+            )
+
+        # self.x_sparse.data = sum((to_arraytype(self._b[0], self.btype)[self.x_sparse.row,:]*to_arraytype(self._b[1], self.btype))*to_arraytype((self._b[2]).T, self.btype)[self.x_sparse.col,:], axis=1)
+
         # print('update_x_sparse', time.time() - tic)
         return None
-        
+
     def update_y(self):
         """
         Update the augmented Lagrangian variable Y.
 
-        Parameters:
+        Parameters
+        ----------
         None
 
-        Returns:
+        Returns
+        -------
         None
         """
-        tic = time.time()
+        time.time()
         self.y.data += self.delta * (self.a.data - self.x_sparse.data)
         # print('Update Y', time.time() - tic)
-        
 
     def update_delta(self):
         """
         Update the step size delta if needed.
 
-        Parameters:
+        Parameters
+        ----------
         None
 
-        Returns:
+        Returns
+        -------
         None
         """
         self.delta *= 1
         return self.delta
 
-    
-    def run(self, a: ty.Array) -> None:
+    def run(self, a: uty.Array) -> None:
         """
         Execute the SVT algorithm.
 
-        Parameters:
+        Parameters
+        ----------
         None
 
-        Returns:
+        Returns
+        -------
         None
         """
         self.initialize(a)
@@ -304,11 +322,15 @@ class svt:
 
             if self.stopping_criterion():
                 break
-            
+
             self.tic = time.time()
             self.update_y()
             self.update_delta()
-            
+
             self.k += 1
 
         return None
+
+
+# for back compatibility with previous versions
+svt = SVT
